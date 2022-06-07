236e32878cae9c20bdd75d70467b54968785c542
BHeinen38 <bailey.heinen@proplanner.com>
2022-06-07 13:10:08 -0500

    Explorer Initial Solution

---
+++ ExplorerApplication/ExplorerApplication/ExplorerForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExplorerApplication
+{
+    public partial class ExplorerForm : Form
+    {
+        public ExplorerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExplorerForm_Load(object sender, EventArgs e)
+        {
+            var docs = new TreeNode("My Documents");
+            docs.Tag = "C:\\Users\bailey.heinen\\Documents";
+           // docs.Tag = System.IO.Path.GetFullPath("C:\\Users\bailey.heinen\\Documents");
+        }
+    }
+}
